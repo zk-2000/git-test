0c06f05b0e554157fef0b9b453bbc96548389789
Zhaokui <zhaokuii11@163.com>
2021-05-10 22:29:58 +0800

    first commit

---
+++ src/main/java/com/cainiao/GitTest.java
@@ -0,0 +1,11 @@
+package com.cainiao;
+
+/**
+ * @author shkstart
+ * @create 2021-05-10 22:20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
