8897c1faf3a51f7788d292a71598bd37e6e06a77
Zhaokui <zhaokuii11@163.com>
2021-05-11 18:10:17 +0800

    second commit

--- src/main/java/com/cainiao/GitTest.java
+++ src/main/java/com/cainiao/GitTest.java
@@ -7,5 +7,6 @@ package com.cainiao;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git second");
     }
 }
